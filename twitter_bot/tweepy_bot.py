983fe1d8fd0c4bcd488818ed97b9b063e4e5cce6
faraji-fuji <farajifuji@gmail.com>
2022-12-24 18:11:10 +0300

    refactor

--- twitter_bot/tweepy_bot.py
+++ twitter_bot/tweepy_bot.py
@@ -1,38 +1,25 @@
 import tweepy
 import json
 
-import requests
 
-def send_message(text_message):
-    payload = {
-    "chat_id":"-1001860660149",
-    "text":f"{text_message}",
-    }
-    url ="https://api.telegram.org/bot5927824100:AAFO2X08IzdO5uNRdRSuMr3wYBre4vWX1xo/sendMessage"
-    result = requests.get(url, params=payload)
-    return None
+def get_bearer_token():
+    '''
+    get bearer token from a json file
+    '''
+    with open("bearer_token.json") as bearer_token_file:
+        data = json.load(bearer_token_file)
+        return data["bearer_token"]
 
 
+def get_user_id(user_name):
+    pass
+    
 
-
-file_object = open('bearer_token.json')
-data = json.load(file_object)
-file_object.close()
-bearer_token = data["bearer_token"]
-
-client = tweepy.Client(bearer_token)
-
-user_id = 22910295
-response = client.get_users_tweets(user_id)
-
-print(response.data)
-print(type(response.data[0]))
-
-
-for tweet in response.data:
-    send_message(tweet)
-    # print(tweet)
-# chelsea_tweets_json = tweets.json
-
-# with open("chelsea_tweets.txt", "w") as chelsea_tweets:
-#     chelsea_tweets.write(myresponse)
+def get_tweets(user_id):
+    '''
+    get tweets from specified user id
+    '''
+    bearer_token = get_bearer_token()
+    client = tweepy.Client(bearer_token)
+    response = client.get_users_tweets(user_id)
+    return response.data
