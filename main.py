8a19f728b202028a29b7c5a714612e0dbe9186f4
faraji-fuji <farajifuji@gmail.com>
2022-12-25 19:58:53 +0300

    bot

--- main.py
+++ main.py
@@ -1,8 +1,53 @@
-import twitter_bot.tweepy_bot as tw
+import twitter_bot.tweepy_bot as tp
+import twitter_bot.tweety as tt
 import telegram_bot.test_bot as tg
+import json
 
 user_id = 22910295
-tweets = tw.get_tweets(user_id)
+tweets = tp.get_tweets(user_id)
 
+tweet_ids = []
 for tweet in tweets:
-    tg.send_message(tweet)
+    tweet_ids.append(tweet.id)
+
+tp.update_ids(tweet_ids)
+
+pending_ids_list = tp.get_pending_ids()
+
+if len(pending_ids_list):
+    url = tt.create_url(pending_ids_list)
+    json_response = tt.connnect_to_endpoint(url)
+    with open("new_tweets.json","w+") as tweets_json_file:
+        tweets_json_file.write(json.dumps(json_response, indent=4, sort_keys=True))
+
+    with open("new_tweets.json") as file:
+        data = json.load(file)
+
+        for item in data["data"]:
+            try: 
+                expanded_url = item["entities"]["urls"][0]["expanded_url"]
+            except:
+                continue
+            tg.send_message(expanded_url)
+    tp.save_posted_ids(pending_ids_list)
+            
+
+
+# tweet_dict = {}
+
+# tweet_ids = set()
+
+# print(tweet_dict)
+    # with open("tweets.txt","w") as tweets_text_file:
+    #     tweets_text_file.write(str(tweet))
+    # tg.send_message(tweet)
+
+# with open("tweets.json", "a+") as json_tweets_file:
+#     json_tweets_file.write(json.dumps(tweet_dict))
+
+
+# with open("tweets.json","r") as tweets_file:
+#     myfile = json.load(tweets_file)
+
+#     for v in myfile:
+#         print(myfile[v)
