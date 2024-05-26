226183df1449551f0f13ab8d28397d36a10a784d
Faraji Ombonya <farajiombonya@gmail.com>
2024-05-26 15:36:17 +0300

    feat: robust run source for SpiderManager

--- spider/utils/managers.py
+++ spider/utils/managers.py
@@ -20,80 +20,77 @@ logger = logging.getLogger(__name__)
 
 
 class SpiderManager():
-    def __init__(self):
-        pass
+    """
+    from spider.utils.managers import SpiderManager
+    m = SpiderManager()            
+    m.run_sources_and_domains(sources=["standard", "star"], domains=["sports", "politics"])
+    """
 
-    def run_standard(self, domain):
-        """Run Standard.
+    STAR = "star"
+    STANDARD = "standard"
 
-        Run Standard, pulling from specified domain.
+    SOURCE_CLASS = {
+        STAR: Star,
+        STANDARD: Standard,
+    }
 
-        Arguments:
-        domain -- The domain to pull from e.g sports, politics
-        """
-        try:
-            standard = Standard()
-            urls = standard.DOMAINS[domain]
-            entries = standard.extract(urls[0])
-            posts = standard.transform_bulk(entries)
-            standard.load_bulk(posts)
-            pending_posts = standard.get_pending_posts(source=Post.STANDARD)
-            telegram_posts = standard.to_telegram_posts(pending_posts)
+    SOURCE_TYPE = {
+        STAR: Post.STAR,
+        STANDARD: Post.STANDARD,
+    }
 
-            telegram = Telegram()
-            domain_channel = {
-                standard.SPORTS: telegram.SPORTS_KENYA,
-                "test": telegram.TEST_CHANNEL
-            }
-            telegram.send_posts(telegram_posts, domain_channel[domain])
-        except NoPendingPostsError:
-            logger.warning("No pending posts from standard")
+    DOMAIN_CHANNEL = {
+        "sports": Telegram.SPORTS_KENYA,
+        "politics": Telegram.KENYAN_POLITICS,
+        "test": Telegram.TEST_CHANNEL,
+    }
 
-    def run_star(self, domain):
-        """Run The Star. 
+    def __init__(self, acknowledge=True):
+        self.acknowledge = acknowledge
 
-        Run The Star, pulling from specified domain.
+    def run_source(self, source, domain):
+        """Run a source pulling content for a particular domain.
 
         Arguments:
-        domain -- The domain to pull from e.g sports, politics
+        source -- content source e.g. star, standard
+        domain -- content domain e.g. politics, sports
         """
+        source_instance = self.SOURCE_CLASS[source]()
+
         try:
-            star = Star()
-            urls = star.DOMAINS[domain]
-            entries = star.extract_bulk(urls)
-            posts = star.transform_bulk(entries)
-            star.load_bulk(posts)
-            pending_posts = star.get_pending_posts(source=Post.STAR)
-            telegram_posts = star.to_telegram_posts(pending_posts)
+            urls = source_instance.DOMAIN_URLS[domain]
+        except Exception as e:
+            logger.info(e)
+            return
 
-            telegram = Telegram()
-            domain_channel = {
-                star.POLITICS: telegram.KENYAN_POLITICS,
-                star.SPORTS: telegram.SPORTS_KENYA,
-                "test": telegram.TEST_CHANNEL,
-            }
-            telegram.send_posts(telegram_posts, domain_channel[domain])
-        except NoPendingPostsError:
-            logger.warning("No pending posts from star")
+        entries = source_instance.extract_bulk(urls)
+        posts = source_instance.transform_bulk(entries)
+        source_instance.load_bulk(posts)
 
-    def run(self):
-        """Run spider manager.
+        pending_posts = source_instance.get_pending_posts(
+            source=self.SOURCE_TYPE[source])
+        telegram_posts = source_instance.to_telegram_posts(pending_posts)
 
-        Run spider manager by calling a source method and a domain.
-        """
-        self.run_standard('sports')
-        self.run_star('sports')
-        self.run_star('politics')
+        telegram = Telegram(acknowledge=self.acknowledge)
+        telegram.send_posts(telegram_posts, self.DOMAIN_CHANNEL[domain])
+
+    def run_sources_and_domains(self, sources, domains):
+        for source in sources:
+            for domain in domains:
+                self.run_source(source=source, domain=domain)
+                time.sleep(random.randint(10, 20))
+            time.sleep(random.randint(20, 30))
 
     def run_forever(self):
         """Run spider manager forever.
 
-        Run spider manager forever by calling a source method and a domain
-        in an infinite loop with random sleep times.
+        Run spider manager forever by calling run_sources_and_domains() 
+        method in an infinite loop with random sleep times.
         """
         logger.info("Running in forever mode.")
         while True:
-            self.run()
+            self.run_sources_and_domains(
+                sources=["standard", "star"], domains=["sports", "politics"])
             logger.info(
                 "Done fetching and sharing posts. Going to sleep for a while.")
             time.sleep(random.randint(600, 3600))
