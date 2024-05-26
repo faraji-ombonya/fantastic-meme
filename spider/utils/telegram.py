16e38343f94f93376e7dd2dc63bafaff335b172c
Faraji Ombonya <farajiombonya@gmail.com>
2024-05-26 14:48:29 +0300

    refactor: reduce indentation level

--- spider/utils/telegram.py
+++ spider/utils/telegram.py
@@ -11,12 +11,13 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s -%(levelname)s - %(message)s')
 
+
 class Telegram():
     SPORTS_KENYA = "sports-kenya"
     KENYAN_POLITICS = "kenyan-politics"
-    TEST_CHANNEL = "test-channel"    
+    TEST_CHANNEL = "test-channel"
 
-    def __init__(self, rate_limited=True):
+    def __init__(self, rate_limited=True, acknowledge=True):
         self.chat_id = settings.TELEGRAM_CHAT_ID
         self.base_url = settings.TELEGRAM_BASE_URL
         self.api_key = settings.TELEGRAM_API_KEY
@@ -27,6 +28,7 @@ class Telegram():
         }
         self.url = f"{self.base_url}/bot{self.api_key}/sendMessage"
         self.rate_limited = rate_limited
+        self.acknowledge = acknowledge
 
     def acknowledge_post(self, post_slug):
         """Acknowledge that a post has been sent to the channel."""
@@ -47,17 +49,23 @@ class Telegram():
 
         try:
             response = requests.get(self.url, params=payload)
-            if response.status_code == 200:
-                logging.info("Message sent successfully.")
-                self.acknowledge_post(post_slug=slug)
-                return post
-            else:
-                logging.info(f"An error occurred. Status: {response.status_code}")
+
+            if response.status_code != 200:
+                logging.info(
+                    f"An error occurred. Status: {response.status_code}")
                 return False
+
+            logging.info("Message sent successfully.")
+
+            if self.acknowledge:
+                self.acknowledge_post(post_slug=slug)
+
+            return post
+
         except:
             logging.error(f"Failed to send message to telegram.")
             return False
-        
+
     def send_posts(self, posts, chanel):
         sent_posts = []
         for post in posts:
@@ -69,13 +77,13 @@ class Telegram():
             if self.rate_limited:
                 time.sleep(random.randint(2, 5))
         return sent_posts
-    
+
     def send_base64_image(self, base64_data, caption=None):
         base64_data = base64_data.replace("data:image/jpg;base64,", "")
         image_data = base64.b64decode(base64_data)
         with open("image.jpg", "wb") as f:
             f.write(image_data)
-            
+
         with open("image.jpg", "rb") as image:
             files = {"photo": image}
             payload = {"chat_id": self.chat_id}
