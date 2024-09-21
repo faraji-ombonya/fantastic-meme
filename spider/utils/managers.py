acbd1217062006fbe6c4b157bca37a08675a736e
Faraji Ombonya <farajiombonya@gmail.com>
2024-09-21 12:01:48 +0300

    refactor: run sources

--- spider/utils/managers.py
+++ spider/utils/managers.py
@@ -13,16 +13,16 @@ from spider.utils.sources.base import (
 )
 
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s')
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 logger = logging.getLogger(__name__)
 
 
-class SpiderManager():
+class SpiderManager:
     """
     from spider.utils.managers import SpiderManager
-    m = SpiderManager()            
+    m = SpiderManager()
     m.run_sources_and_domains(sources=["standard", "star"], domains=["sports", "politics"])
     """
 
@@ -52,8 +52,8 @@ class SpiderManager():
         """Run a source pulling content for a particular domain.
 
         Arguments:
-        source -- content source e.g. star, standard
-        domain -- content domain e.g. politics, sports
+        source -- an object representing a content source e.g. star, standard
+        that has the name of the source and the domains to pull from
         """
         logger.info(f"Running source: {source}, Domain: {domain}")
         source_instance = self.SOURCE_CLASS[source]()
@@ -74,29 +74,34 @@ class SpiderManager():
         source_instance.load_bulk(posts)
 
         pending_posts = source_instance.get_pending_posts(
-            source=self.SOURCE_TYPE[source])
+            source=self.SOURCE_TYPE[source]
+        )
         telegram_posts = source_instance.to_telegram_posts(pending_posts)
 
         telegram = Telegram(acknowledge=self.acknowledge)
         telegram.send_posts(telegram_posts, self.DOMAIN_CHANNEL[domain])
 
-    def run_sources_and_domains(self, sources, domains):
+    def run_sources(self, sources):
         for source in sources:
-            for domain in domains:
-                self.run_source(source=source, domain=domain)
+            source_name = source.get("name")
+            source_domains = source.get("domains")
+            for domain in source_domains:
+                self.run_source(source=source_name, domain=domain)
                 time.sleep(random.randint(10, 20))
             time.sleep(random.randint(20, 30))
 
     def run_forever(self):
         """Run spider manager forever.
 
-        Run spider manager forever by calling run_sources_and_domains() 
+        Run spider manager forever by calling run_sources()
         method in an infinite loop with random sleep times.
         """
         logger.info("Running in forever mode.")
+        sources = [
+            {"name": "standard", "domains": ["sports", "politics"]},
+            {"name": "star", "domains": ["sports", "politics"]},
+        ]
         while True:
-            self.run_sources_and_domains(
-                sources=["standard", "star"], domains=["sports", "politics"])
-            logger.info(
-                "Done fetching and sharing posts. Going to sleep for a while.")
+            self.run_sources(sources=sources)
+            logger.info("Done fetching and sharing posts. Going to sleep for a while.")
             time.sleep(random.randint(600, 3600))
